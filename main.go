be122bdde0da3c8a9e058fcb6188238f098b0398
Alaa Yousfi <alaaelusfy@gmail.com>
2026-04-07 00:02:02 +0100

    Add amqp package, Connect the producer by creating a connection, create channel, create queue, publish a message to the queue

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Go RabbitMQ Tutorial")
+
+	conn, err := amqp.Dial("amqp://user:password@localhost:5672/")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer conn.Close()
+	fmt.Println("Successfully Connected To RabbitMQ Instance")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare("TestQueue", false, false, false, false, nil)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	fmt.Println(q)
+
+	err = ch.Publish("", "TestQueue", false, false, amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte("Hello World"),
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println("Successfully Published Message to Queue")
 }
